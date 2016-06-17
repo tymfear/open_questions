e1266a3fd92eeadb40e3a3066c092f2f7e1587c9
Tymur Daudov <tymfear@gmail.com>
2016-06-17 17:56:19 +0300

    continuing

--- db/schema.rb
+++ db/schema.rb
@@ -28,8 +28,9 @@ ActiveRecord::Schema.define(version: 20160617145203) do
     t.string   "name"
     t.string   "git_name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
